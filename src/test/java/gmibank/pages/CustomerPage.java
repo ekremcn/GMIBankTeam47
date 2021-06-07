b3f2a67dd04715742d98b76b8de4ee5e6df47af1
harunerdogan387 <harunerdogan387@gmail.com>
2021-06-06 20:33:42 -0500

    first commit

--- src/test/java/gmibank/pages/CustomerPage.java
+++ src/test/java/gmibank/pages/CustomerPage.java
@@ -1,6 +1,9 @@
 package gmibank.pages;
 
-public class CustomerPage {
+import gmibank.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
 
+public class CustomerPage {
+    public CustomerPage(){  PageFactory.initElements(Driver.getDriver(),this); }
 
 }
