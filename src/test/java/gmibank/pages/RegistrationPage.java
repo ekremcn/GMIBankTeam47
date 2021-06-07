d4c92f9d58ecb157b63e59b2f54b8386e68c5a01
erhankale <erhankal3@gmail.com>
2021-06-08 00:38:36 +0200

    MainPage, Hooks, RegistrationPage, Feature, StepDefs, cucumber.properties added or updated

--- src/test/java/gmibank/pages/RegistrationPage.java
+++ src/test/java/gmibank/pages/RegistrationPage.java
@@ -1,7 +1,13 @@
 package gmibank.pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RegistrationPage extends MainPage {
 
@@ -35,4 +41,45 @@ public class RegistrationPage extends MainPage {
     @FindBy(id="register-submit")
     public WebElement btnRegister;
 
+    @FindAll({
+            @FindBy(xpath = "//input/../*[3]")
+    })
+    public List<WebElement> feedbacks;
+
+    public void whichBoxIsMissing(){
+        ArrayList<String> feedbacksTexts = new ArrayList<String>();
+        ArrayList<String> expectedList = new ArrayList<>(Arrays.asList(
+                "SSN", "First Name", "Last Name",
+                "Address", "Mobile Number", "Username",
+                "Email", "Password First Box", "Password Second Box" ));
+        for (WebElement each : feedbacks) {
+            feedbacksTexts.add(each.getText());
+            System.out.println(each.getText());
+            if(each.getText().contains("SSN")){
+                expectedList.remove("SSN");
+            }else if(each.getText().contains("First")){
+                expectedList.remove("First Name");
+            }else if(each.getText().contains("Last")){
+                expectedList.remove("Last Name");
+            }else if(each.getText().contains("address")){
+                expectedList.remove("Address");
+            }else if(each.getText().contains("mobile")){
+                expectedList.remove("Mobile Number");
+            }else if(each.getText().contains("username")){
+                expectedList.remove("Username");
+            }else if(each.getText().contains("email")){
+                expectedList.remove("Email");
+            }else if(each.getText().contains("Your password")){
+                expectedList.remove("Password First Box");
+            }else if(each.getText().contains("confirmation")){
+                expectedList.remove("Password Second Box");
+            }
+        }
+
+        if(expectedList.size()>0){
+            System.out.println( expectedList.toString()+" feedbacks is missing");
+        }else{
+            System.out.println("All input boxes was filled successfully");
+        }
+    }
 }
